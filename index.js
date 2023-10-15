215acb5caed02d1c29cbe9240863bdea4ab6a2db
Sarvasva Gupta <sarvasv2004@gmail.com>
2023-10-15 14:50:14 +0530

    Add files for version 1.1 via upload

--- index.js
+++ index.js
@@ -8,40 +8,47 @@ var taskPArr = [];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-function checkNonEmpty(task){
-    if(task.length===0)
-    return false;
+function checkNonEmpty(task) {
+    if (task.length === 0)
+        return false;
 
-    for(i=0;i<task.length;i++)
-    if(task[i]!=' ')
-    return true;
+    for (i = 0; i < task.length; i++)
+        if (task[i] != ' ')
+            return true;
 
     return false;
 }
 
 app.get("/", (req, res) => {
+    taskPArr=[];
+    taskWArr=[];
+    
+    console.log(req.body+"1");
     res.render("index.ejs");
 });
 
 app.get("/work", (req, res) => {
+    console.log(req.body+"2");
     res.render("work.ejs");
 });
 
 app.get("/personal", (req, res) => {
+    console.log(req.body+"3");
     res.render("personal.ejs");
 });
 
 app.post("/", (req, res) => {
+    console.log(req.body+"4");
     res.render("index.ejs");
 });
 
 app.post("/personal", (req, res) => {
-    console.log(req.body);
+    console.log(req.body+"5");
     const task = req.body.task;
-    const del_=req.body.delete;
+    const del_ = req.body.delete;
 
-    if(del_!==undefined)
-    taskPArr=[];
+    if (del_ !== undefined)
+        taskPArr = [];
 
     else if (task !== undefined && checkNonEmpty(task))
         taskPArr.push(task);
@@ -51,19 +58,21 @@ app.post("/personal", (req, res) => {
 
 
 app.get('/success-p', (req, res) => {
+    console.log(req.body+"6");
     res.render("personal.ejs", { task: taskPArr, });
 });
 
 
 app.post("/work", (req, res) => {
+    console.log(req.body+"7");
     const task = req.body.task;
-    const del_=req.body.delete;
+    const del_ = req.body.delete;
     console.log(req.body);
 
-    if(del_!==undefined)
-    taskWArr=[];
+    if (del_ !== undefined)
+        taskWArr = [];
 
-    else if (task !== undefined&& checkNonEmpty(task))
+    else if (task !== undefined && checkNonEmpty(task))
         taskWArr.push(task);
     console.log(task);
 
@@ -71,6 +80,7 @@ app.post("/work", (req, res) => {
 });
 
 app.get('/success-w', (req, res) => {
+    console.log(req.body+"8");
     res.render("work.ejs", { task: taskWArr, });
 });
 
